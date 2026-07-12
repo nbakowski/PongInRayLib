9f77cc504aeea028d4f84132c9b0fda35465cc00
Nikodem Bąkowski <n.bakowski@icloud.com>
2026-07-12 23:10:19 +0200

    Moved to global screen variables.

--- PongGame/PongGame.cpp
+++ PongGame/PongGame.cpp
@@ -6,32 +6,52 @@
 #include <array>
 #include <string>
 
+inline constexpr int default_window_width = 1280;
+inline constexpr int default_window_height = 1024;
+inline constexpr int min_window_width = 640;
+inline constexpr int min_window_height = 480;
 inline constexpr int score_font_size = 96;
 inline constexpr int pause_text_size = 48;
 inline constexpr int text_padding = 12;
 inline constexpr int line_thickness = 6;
 
+int screen_width = default_window_width;
+int screen_height = default_window_height;
+
 namespace
 {
-    void restart_the_game(auto& players, ball& b, const int s_w, const int s_h)
+    void reset_players_position(player& p1, player& p2)
+    {
+        p1.set_player_position(
+            0,
+            (screen_height - p1.get_player_height()) / 2.0f
+        );
+
+        p2.set_player_position(
+            screen_width - p2.get_player_width(),
+            (screen_height - p2.get_player_height()) / 2.0f
+        );
+    }
+
+    void restart_the_game(auto& players, ball& b)
     {
         for (player* p : players)
         {
             p->reset_points();
         }
-        b.reset_ball_position(s_w, s_h);
+        b.reset_ball_position(screen_width, screen_height);
     }
 
-    void render_dashed_line(const int s_w, const int s_h)
+    void render_dashed_line()
     {
         constexpr int rec_height = 20;
-        for (auto i = 0; i < s_h; i += rec_height * 2)
+        for (auto i = 0; i < screen_height; i += rec_height * 2)
         {
-            DrawRectangle((s_w - line_thickness) / 2, i, line_thickness, rec_height, BLACK);
+            DrawRectangle((screen_width - line_thickness) / 2, i, line_thickness, rec_height, BLACK);
         }
     }
 
-    void draw_the_score(const int p_one_points, const int p_two_points, const int width)
+    void draw_the_score(const int p_one_points, const int p_two_points)
     {
         DrawText(
             std::to_string(p_one_points).c_str(),
@@ -42,7 +62,7 @@ namespace
         );
         DrawText(
             std::to_string(p_two_points).c_str(),
-            width - MeasureText(std::to_string(p_two_points).c_str(),
+            screen_width - MeasureText(std::to_string(p_two_points).c_str(),
                 score_font_size) - text_padding,
             text_padding,
             score_font_size,
@@ -53,29 +73,22 @@ namespace
 
 int main()
 {
-    constexpr int screen_width = 1280;
-    constexpr int screen_height = 1024;
     bool is_game_paused = false;
+    bool is_shader_loaded = false;
 
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(screen_width, screen_height, "SigmaPong");
+    SetWindowMinSize(min_window_width, min_window_height);
     InitAudioDevice();
     SetTargetFPS(60);
     SetTraceLogLevel(LOG_ERROR);
 
-
     player player_one(KEY_W, KEY_S);
     player player_two(KEY_UP, KEY_DOWN);
+    
     ball ball(screen_width / 2.0f, screen_height / 2.0f, 10.0f, RED);
 
-    player_one.set_player_position(
-        0, 
-        (screen_height - player_one.get_player_height()) / 2.0f
-    );
-
-    player_two.set_player_position(
-        screen_width - player_two.get_player_width(), 
-        (screen_height - player_one.get_player_height()) / 2.0f
-    );
+    reset_players_position(player_one, player_two);
 
     const std::array players = {&player_one, &player_two};
 
@@ -90,11 +103,24 @@ int main()
     {
         TraceLog(LOG_ERROR, "Failed to load the shader.");
     }
+    else
+    {
+        is_shader_loaded = true;
+    }
     
-    const RenderTexture2D canvas = LoadRenderTexture(screen_width, screen_height);
+    RenderTexture2D canvas = LoadRenderTexture(screen_width, screen_height);
 
     while (!WindowShouldClose())
     {
+        if (IsWindowResized())
+        {
+            UnloadRenderTexture(canvas);
+            screen_width = GetScreenWidth();
+            screen_height = GetScreenHeight();
+            canvas = LoadRenderTexture(screen_width, screen_height);
+            reset_players_position(player_one, player_two);
+            ball.reset_ball_position(screen_width, screen_height);
+        }
 
         BeginDrawing();
 
@@ -105,7 +131,12 @@ int main()
         
         if (IsKeyPressed(KEY_R))
         {
-            restart_the_game(players, ball, screen_width, screen_height);
+            restart_the_game(players, ball);
+        }
+        
+        if (IsKeyPressed(KEY_F11))
+        {
+            ToggleBorderlessWindowed();
         }
 
         if (!is_game_paused)
@@ -149,11 +180,11 @@ int main()
         BeginTextureMode(canvas);
 
     		ClearBackground(RAYWHITE);
-            render_dashed_line(screen_width, screen_height);
+            render_dashed_line();
 	        player_one.render_player();
 	        player_two.render_player();
 	        ball.render_ball();
-            draw_the_score(player_one.get_points(), player_two.get_points(), screen_width);
+            draw_the_score(player_one.get_points(), player_two.get_points());
             if (is_game_paused)
             {
                 DrawText(
@@ -167,26 +198,28 @@ int main()
 
         EndTextureMode();
 
-        BeginShaderMode(crt);
-			const Rectangle rec =
-			{
-				.x = 0, 
-				.y = 0, 
-				.width = static_cast<float>(canvas.texture.width), 
-				.height = static_cast<float>(-canvas.texture.height)
-			};
-
-            constexpr Vector2 vec =
+        if (is_shader_loaded) BeginShaderMode(crt);
+            const Rectangle rec =
             {
                 .x = 0,
-                .y = 0
-			};
-			DrawTextureRec(canvas.texture, rec, vec, WHITE);
-        EndShaderMode();
+                .y = 0,
+                .width = static_cast<float>(canvas.texture.width),
+                .height = static_cast<float>(-canvas.texture.height)
+            };
+
+            const Vector2 vec =
+            {
+                .x = (screen_width - canvas.texture.width) / 2.0f,
+                .y = (screen_height - canvas.texture.height) / 2.0f
+            };
+
+            DrawTextureRec(canvas.texture, rec, vec, WHITE);
+        if (is_shader_loaded) EndShaderMode();
 
         EndDrawing();
     }
 
+    CloseAudioDevice();
     UnloadRenderTexture(canvas);
     UnloadShader(crt);
     CloseWindow();
